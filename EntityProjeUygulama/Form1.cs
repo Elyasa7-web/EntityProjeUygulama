6663337c04a84fb3964e67acc5d31cf6615b49a5
fb_20 <fb_20@ELYASA>
2024-12-27 23:14:27 +0300

    Add project files.

---
+++ EntityProjeUygulama/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Spatial;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityProjeUygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DbEntityUrunEntities db = new DbEntityUrunEntities();
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            var kategoriler = db.TBLKATEGORİ.ToList();
+            dataGridView1.DataSource = kategoriler;
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            TBLKATEGORİ t = new TBLKATEGORİ();
+            t.AD = textBox2.Text;
+            db.TBLKATEGORİ.Add(t);
+            db.SaveChanges();
+            MessageBox.Show("Kategori eklendi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            int x=Convert.ToInt32(textBox1.Text);
+            var ktgr = db.TBLKATEGORİ.Find(x);
+            db.TBLKATEGORİ.Remove(ktgr);
+            db.SaveChanges();
+            MessageBox.Show("Kategori silindi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(textBox1.Text);
+            var ktgr = db.TBLKATEGORİ.Find(x);
+            ktgr.AD = textBox2.Text;
+            db.SaveChanges();
+            MessageBox.Show("Kategori güncellendi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ EntityProjeUygulama/FrmUrun.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityProjeUygulama
+{
+    public partial class FrmUrun : Form
+    {
+        public FrmUrun()
+        {
+            InitializeComponent();
+        }
+        DbEntityUrunEntities db = new DbEntityUrunEntities();
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = (from x in db.TBLURUN
+                                        select new
+                                        {
+                                            x.URUNID,
+                                            x.URUNAD,
+                                            x.MARKA,
+                                            x.STOK,
+                                            x.FİYAT,
+                                            x.DURUM,
+                                            x.TBLKATEGORİ.AD
+                                        }).ToList();
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            TBLURUN t = new TBLURUN();
+            t.URUNAD = TxtUrunAd.Text;
+            t.MARKA= TxtMarka.Text;
+            t.STOK = short.Parse(TxtStok.Text);
+            t.KATEGORİ = int.Parse(comboBox1.SelectedValue.ToString());
+            t.FİYAT = decimal.Parse(TxtFıyat.Text);
+            t.DURUM = true;
+            db.TBLURUN.Add(t);
+            db.SaveChanges();
+            MessageBox.Show("Ürün Sisteme Kaydedildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(TxtID.Text);
+            var urun = db.TBLURUN.Find(x);
+            db.TBLURUN.Remove(urun);
+            db.SaveChanges();
+            MessageBox.Show("Ürün Silindi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(TxtID.Text);
+            var urun = db.TBLURUN.Find(x);
+            urun.URUNAD = TxtUrunAd.Text;
+            urun.MARKA = TxtMarka.Text;
+            urun.STOK = short.Parse(TxtStok.Text);
+            db.SaveChanges();
+            MessageBox.Show("Ürün Güncellendi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void FrmUrun_Load(object sender, EventArgs e)
+        {
+            var kategoriler = (from x in db.TBLKATEGORİ
+
+                               select new
+                               {
+                                   x.ID,
+                                   x.AD
+                               }
+                               ).ToList();
+            comboBox1.ValueMember = "ID";
+            comboBox1.DisplayMember = "AD";
+            comboBox1.DataSource = kategoriler;
+        }
+    }
+}
